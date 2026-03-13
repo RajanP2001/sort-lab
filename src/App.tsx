a669f41bba4333665d102e36e6d315b5b210d86e
Rajan Patel <rajan2001.patel@gmail.com>
2026-03-13 03:23:33 -0500

    feature: generate and render random arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import { generateRandomArray } from "./utils/generateRandomArray";
 
-const sampleBars = [80, 140, 60, 180, 110, 95, 150, 70, 130, 50, 165, 100];
+const ARRAY_SIZE = 36;
+const MIN_VALUE = 5;
+const MAX_VALUE = 100;
 
 function App() {
+  const [array, setArray] = useState<number[]>(() =>
+    generateRandomArray(ARRAY_SIZE, MIN_VALUE, MAX_VALUE),
+  );
+
+  function handleGenerateArray() {
+    setArray(generateRandomArray(ARRAY_SIZE, MIN_VALUE, MAX_VALUE));
+  }
+
   return (
     <div className="app">
       <header className="app__header">
@@ -20,19 +32,18 @@ function App() {
           </select>
         </div>
 
-        <button disabled>Generate New Array</button>
+        <button onClick={handleGenerateArray}>Generate New Array</button>
         <button disabled>Start Sorting</button>
       </section>
 
       <section className="visualizer">
-        {sampleBars.map((value, index) => (
+        {array.map((value, index) => (
           <div key={index} className="bar-wrapper">
             <div
               className="bar"
-              style={{ height: `${value * 2}px` }}
+              style={{ height: `${value * 3}px` }}
               title={`Value: ${value}`}
             />
-            <span className="bar-label">{value}</span>
           </div>
         ))}
       </section>
